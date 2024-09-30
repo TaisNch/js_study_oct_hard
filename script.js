60ba083eb4d6d0ca39cae3ca1f990dfe2c249c71
TaisNch <tais.aliexpress@gmail.com>
2024-09-30 11:52:11 +0200

    lesson03hard

--- script.js
+++ script.js
@@ -1,23 +1,53 @@
-// 1) Создать переменную num со значением 266219 (тип данных число)
-const num = 266219;
+// 1). Переменная lang может принимать 2 значения: 'ru' 'en'.
+// const lang = prompt('Выберите язык - ru или en', 'ru'); 
 
-// 2) Вывести в консоль произведение (умножение) цифр этого числа
-const digits = num.toString();
-console.log(digits);
-let product = 1;
-for (let i = 0; i < digits.length; i++) {
-   product *= Number(digits[i]);
+// Написать условия при котором в зависимости от значения lang будут выводится дни недели на русском или английском языке. Решите задачу
+
+//  a) через if,
+const langIf = prompt('Выберите язык - ru или en', 'ru'); 
+const daysRu = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
+const daysEn = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+if (langIf === 'ru') {
+  console.log(daysRu);
+} else if (langIf === 'en') {
+  console.log(daysEn);
+} else {
+  console.log('Неверный ввод языка');
 }
-console.log(product);
 
-// 3) Полученный результат возвести в степень 3, используя только 1 оператор (Math.pow не подходит)
-const result = product*product*product;
+//  b) через switch-case
+const langSwitch = prompt('Выберите язык - ru или en', 'ru'); 
+switch (langSwitch) {
+  case 'ru':
+    console.log(daysRu);
+    break;
+  case  'en':
+      console.log(console.log(daysEn));
+      break;
+  default:
+      console.log('Неверный ввод языка');
+}
 
-console.log(result);
+//  c) через многомерный массив без ифов и switch.
+const days = {
+  'ru': ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'],
+  'en': ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+};
 
-// 4) Вывести в консоль первые 2 цифры полученного числа
-const digitRslt = result.toString();
-console.log(digitRslt.substring(0,2));
+// Переменная lang, которая может принимать значения 'ru' или 'en'
+const langArr = prompt('Выберите язык - ru или en', 'ru');
 
+// Выводим соответствующий массив дней недели в зависимости от выбранного языка
+console.log(days[langArr] || 'Неверный ввод языка');
+
+// 2). У нас есть переменная namePerson. Если значение этой переменной “Артем” то вывести в консоль “директор”, если значение “Александр” то вывести в консоль “преподаватель”, с любым другим значением вывести в консоль “студент”
+
+const namePerson = prompt('Введите имя', 'Артем');
+
+//  Решить задачу с помощью нескольких тернарных операторов, без использования if или switch
+
+const role = (namePerson === 'Артем') ? console.log('директор') :
+             (namePerson === 'Александр') ? console.log('преподаватель') :
+             console.log('студент');
 
 
