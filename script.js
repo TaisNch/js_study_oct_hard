db9ce871e82723cc652c171dc131aa22066c6d03
TaisNch <tais.aliexpress@gmail.com>
2024-10-07 15:51:29 +0200

    lesson04v1

--- script.js
+++ script.js
@@ -1,35 +1,11 @@
-// 1) Создать массив arr = []
-const arr= []
-// — Записать в него 7 любых многозначных чисел в виде строк
-arr.push ('45678', '987654', '1234567', '7654321', '23456789', '87654321', '34567890');
-console.log (arr);
-// — Вывести в консоль только те, что начинаются с цифры 2 или 4 (Должны присутствовать в массиве)
-let arrSort = []
-for (let i=0; i<=6;i++) {
- let number = arr[i].substring(0,1)
-  if (number ==='7' ||number ==='2'  ||number ==='4') {
-    arrSort.push (arr[i])
+// 1) Создайте функцию, которая принимает 1 аргумент (название произвольное)
+const newItem = function newExpr(item) {
+  // Проверка, является ли аргумент строкой
+  if (typeof item !== 'string') {
+    return 'У Вас ошибка, нужно ввести строку';
   }
- }
- console.log (arrSort);
-
-// 2) Вывести в столбик все простые числа от 1 до 100 (сделать при помощи цикла)
-
-let arrPrime = [];
-const isPrime = function(num) {
-  if (num<=1) {
-    return false
-  }  
-  for (let i=2; i<Math.sqrt(num); i++) {
-    if (num%i === 0) {return false}
-  } 
-  
-  return true}
-
-
-for (let i=1; i<=100;i++) {
-   if(isPrime(i)) { arrPrime.push(`${i}: Делители этого числа: 1 и ${i}`);};
+  const noSpaces = item.trim();
+  if (noSpaces.length > 30) {
+    const trimmedText = noSpaces.substring(0, 30);
+    return `${trimmedText}...`; 
   }
- console.log (arrPrime);
-
-
