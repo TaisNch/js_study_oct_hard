1f65a0810f0de231e05c024f6ea4b5ea23d6ca1c
TaisNch <tais.aliexpress@gmail.com>
2024-10-18 15:40:49 +0200

    lesson09

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
-// 1) Создать массив week и записать в него дни недели в виде строк
-// Вывести на экран все дни недели
-// Каждый из них с новой строчки
-// Выходные дни - курсивом
-// Текущий день - жирным шрифтом(использовать объект даты)
+// Необходимо выполнить в отдельном js файле, подключенному к отдельной html странице
 
-week = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
+// 1) Выведите на страницу текущую дату и время в 2-х форматах:
+//     a) 'Сегодня Вторник, 4 февраля 2020 года, 21 час 5 минут 33 секунды'
+//     б) '04.02.2020 - 21:05:33'
+newTime1 = document.getElementById('newTime1');
+newTime2 = document.getElementById('newTime2');
+newTime3 = document.getElementById('newTime3');
 
-for (let i=0; i<=week.length; i++) {
-  if (i===0 || i === 6) {
-    document.write (`<i>${week[i]}</i></br>`)
-  } else
-  document.write (`${week[i]}</br>`)
+week = ['воскресенье', "понедельник", "вторник","среда","четверг","пятница","суббота"];
+const months = [
+  'Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
+  'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'
+];
+const currentDate = new Date();
+const dayOfWeek = currentDate.getDay(); 
+const day = currentDate.getDate();      //дата   
+const month = currentDate.getMonth() + 1;  
+const year = currentDate.getFullYear();  
+const hours = currentDate.getHours();
+const minutes = currentDate.getMinutes();
+const seconds = currentDate.getSeconds();
+// 2) Для вывода в формате (а) напишите функцию, которая будет менять склонение слов в зависимости от числа, "час, часов, часа"
+let chas ;
+if (+hours == 1) {chas ='час'}
+else if (+hours >= 2 && +hours <= 4) {chas ='часа'}
+else {chas ='часов'};
+newTime1.innerHTML  = `<h2>Сегодня ${week[dayOfWeek]}, ${day} ${months[month]} ${year},${hours} ${chas} ${minutes} мин ${seconds} сек </h2>`;
+
+
+// 3) Для вывода в формате (б) напишите функцию, которая будет добавлять 0 перед значениями которые состоят из одной цифры (из 9:5:3 1.6.2019 сделает 09:05:03 01.06.2019)
+function addLeadingZero(value) {
+  return value < 10 ? '0' + value : value;
 }
-  
-const today = new Date();
-const dayOfWeek = today.getDay(); 
-console.log('Сегодня:', week[dayOfWeek]); 
+newTime2.innerHTML = `<h2>${addLeadingZero(day)}.${addLeadingZero(month)}.${year} -${addLeadingZero(hours)}.${addLeadingZero(minutes)}.${addLeadingZero(seconds)}</h2>`
 
-console.log(week);
+// 4) С помощью функции setInterval, реализуйте обновление даты и времени каждую секунду
+function showTimeInSec(){
+  newTime3.innerHTML =`<h2>${addLeadingZero(day)}.${addLeadingZero(month)}.${year} -${addLeadingZero(hours)}.${addLeadingZero(minutes)}.${addLeadingZero(seconds)}</h2>`
+}
+showTimeInSec();
+setInterval(showTimeInSec(), 1000);
+
